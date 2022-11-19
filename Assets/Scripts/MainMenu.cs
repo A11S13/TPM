f6ca149c30916d07c802fc836110f4d6d1805503
A11S13 <a.shuripa@gmail.com>
2022-11-19 14:31:52 +0200

    Upload

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private FloatSO damageSO;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("main_game", LoadSceneMode.Single);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void God()
+    {
+        damageSO.Value = 0.1f;
+    }
+    public void Easy()
+    {
+        damageSO.Value = 0.2f;
+    }
+
+    public void Normal()
+    {
+        damageSO.Value = 1f;
+    }
+
+    public void Hard()
+    {
+        damageSO.Value = 3f;
+    }
+
+    public void Expert()
+    {
+        damageSO.Value = 10f;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController _controller;
+
+    [SerializeField]
+    private Camera _followCamera;
+    public float movespeed = 5f;
+    public float gravity = -20;
+    public float rotationSpeed = 5;
+
+    Vector3 pos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 input = Quaternion.Euler(0, _followCamera.transform.eulerAngles.y, 0) * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 movementDirection = input.normalized;
+
+        if(movementDirection != Vector3.zero)
+        {
+            Quaternion desiredRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, rotationSpeed * Time.deltaTime);
+        }
+        movementDirection.y += gravity * Time.deltaTime;
+        _controller.Move(movementDirection * movespeed * Time.deltaTime);
+    }
+
+    
+}
